62d5bedfc777b3f2cb6e87f831485ef5cc9bda66
Yael Luria <yaeluria@gmail.com>
2022-06-14 16:12:50 +0300

    Code review fixes - Refactor and fix failing on larger shift_num

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,15 +1,28 @@
+LETTERS_ARRAY = ('a'..'z').to_a
 
-LETTERS_ARRAY = ("a".."z").to_a
+# @param [Array] array - array on which we search the new index
+# @param [Integer] index - starting index
+# @param [Integer] shift_num - integer saying how much to shift the index (right)
+#
+# @return [Integer] shifted index
+#
+def circular_shift_index(array, index, shift_num)
+  (index + shift_num) % array.length
+end
+
+def caesar_cipher_char(char, shift_num)
+  letter_index = LETTERS_ARRAY.index(char.downcase)
+  return char unless letter_index
+
+  shifted_index = circular_shift_index(LETTERS_ARRAY, letter_index, shift_num)
+  ciphered_lowercase_char = LETTERS_ARRAY[shifted_index]
+  char == char.upcase ? ciphered_lowercase_char.upcase : ciphered_lowercase_char
+end
 
 def caesar_cipher(string, shift_num)
-    chars_array = string.chars
-    ciphered_array = chars_array.map  do |char|
-        if LETTERS_ARRAY.index(char.downcase)
-            ciphered_index = LETTERS_ARRAY.index(char.downcase) + shift_num
-            ciphered_lowercase_char = LETTERS_ARRAY[ciphered_index] || LETTERS_ARRAY[ciphered_index - LETTERS_ARRAY.length]
-            char == char.upcase ? ciphered_lowercase_char.upcase : ciphered_lowercase_char
-        else char
-        end
-    end
-    ciphered_array.join
+  chars_array = string.chars
+  ciphered_array = chars_array.map do |char|
+    caesar_cipher_char(char, shift_num)
+  end
+  ciphered_array.join
 end
