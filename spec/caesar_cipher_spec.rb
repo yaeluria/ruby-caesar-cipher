c0c7e862183ffb165ed32d95b1bcd559534e295e
Yael Luria <yaeluria@gmail.com>
2022-06-27 17:03:01 +0300

    Refactor caesar_cipher tests using context and let

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,16 +1,41 @@
 require './caesar_cipher'
 
 describe '#caesar_cipher' do
-  it 'returns a string with all letters shifted by a factor' do
-    expect(caesar_cipher('incredible', 2)).to eql('kpetgfkdng')
+  let(:string) { 'incredible' }
+  let(:expected_result) { 'kpetgfkdng' }
+  let(:shift_num) { 2 }
+  let(:method_result) { caesar_cipher(string, shift_num) }
+
+  it { expect(method_result).to eq expected_result }
+
+  context 'when shift_num is bigger than the letters count' do
+    let(:letters_count) { 26 }
+    let(:shift_num) { 2 + letters_count * 3 }
+
+    it { expect(method_result).to eq expected_result }
   end
-  it 'returns a string with all letters shifted by a factor larger than the alphabet size' do
-    expect(caesar_cipher('incredible', 29)).to eql('lqfuhgleoh')
+  context 'when shift_num is negative' do
+    let(:shift_num) { -2 }
+    let(:expected_result) { 'glapcbgzjc' }
+
+    it { expect(method_result).to eq expected_result }
   end
-  it 'returns a string with the letters shifted and punctuation left in place' do
-    expect(caesar_cipher('What a string, incredible!', 2)).to eql('Yjcv c uvtkpi, kpetgfkdng!')
+  context 'when string contains non alphabetic characters' do
+    let(:string) { 'what a string, incredible!' }
+    let(:expected_result) { 'yjcv c uvtkpi, kpetgfkdng!' }
+
+    it { expect(method_result).to eq expected_result }
   end
-  it 'returns a string with the letters shifted and case preserved' do
-    expect(caesar_cipher('IncrEdible', 2)).to eql('KpetGfkdng')
+  context 'when string contains uppercase letters' do
+    let(:string) { 'IncrEdible' }
+    let(:expected_result) { 'KpetGfkdng' }
+
+    it { expect(method_result).to eq expected_result }
+  end
+  context 'when string is empty' do
+    let(:string) { '' }
+    let(:expected_result) { '' }
+
+    it { expect(method_result).to eq expected_result }
   end
 end
